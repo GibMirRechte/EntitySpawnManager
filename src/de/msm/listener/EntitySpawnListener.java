961b4af0a746648be2f8df0e3bdd2cac7734f93c
GibMirRechte <55894240+DiesesForum@users.noreply.github.com>
2021-01-06 15:41:41 +0100

    Add first version

---
+++ src/de/msm/listener/EntitySpawnListener.java
@@ -0,0 +1,35 @@
+package de.msm.listener;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+public class EntitySpawnListener implements Listener {
+	
+	File file = new File("plugins//EntitySpawnHandler//EntityList.yml");
+	YamlConfiguration yaml = YamlConfiguration.loadConfiguration(file);
+	
+	@EventHandler
+	public void onSpawn(EntitySpawnEvent event) {
+		if(yaml.contains(event.getEntityType().toString())) {
+			if(!yaml.getBoolean(event.getEntityType().toString())) {
+				event.setCancelled(true);
+			}
+			return;
+		}
+		
+		if(event.getEntityType().equals(EntityType.PLAYER)) {
+			return;
+		}
+		
+		yaml.set(event.getEntityType().toString(), true);
+		try { yaml.save(file); }catch (IOException e) {
+		}
+	}
+
+}
---
+++ src/de/msm/main/MobSpawnManager.java
@@ -0,0 +1,26 @@
+package de.msm.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.msm.listener.EntitySpawnListener;
+
+public class MobSpawnManager extends JavaPlugin {
+	
+	public void onEnable() {
+		
+		Bukkit.getPluginManager().registerEvents(new EntitySpawnListener(), this);
+		
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println(" ");
+		System.out.println("========== MobSpawn Manager ==========");
+		System.out.println(" ");
+		System.out.println("Author: GibMirRechte");
+		System.out.println("Version: 1.0");
+		System.out.println("Discord: yhBX6KT");
+		System.out.println(" ");
+		System.out.println("========== MobSpawn Manager ==========");
+	}
+
+}
